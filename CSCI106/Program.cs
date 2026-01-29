6c211763f448676412c4222a62b0bb6dbe2a2fcc
xking0123 <xking012344@gmail.com>
2026-01-29 17:20:20 -0500

    attempting :(

--- CSCI106/Program.cs
+++ CSCI106/Program.cs
@@ -4,13 +4,14 @@
     {
         static void Main()
         {
-            var svg = SvgBuilder.New((500, 500)).Build();
+            var svg = SvgBuilder.New((500, 500));
 
-            Console.Write("Absolute path to save SVG at: ");
-            var path = Console.ReadLine() ?? "";
+            svg.makeRect(50, 50, 20, 7);
+
+            var path = "C:\\Users\\Xavier Harding\\csci-106\\CSCI106\\test.svg";
 
             using (var fileWriter = FileWriter.FromAbsolutePath(path))
-                fileWriter.WriteLine(svg);
+                fileWriter.WriteLine(svg.Build());
         }
     }
 }
--- CSCI106/SvgBuilder.cs
+++ CSCI106/SvgBuilder.cs
@@ -5,6 +5,12 @@
         private const string SVG_HEADER_TEMPLATE = "<svg width=\"{0}\" height=\"{1}\" xmlns=\"http://www.w3.org/2000/svg\">";
         private const string SVG_FOOTER = "</svg>";
 
+        public void makeRect(int x, int y, int width, int height)
+        {
+            Buffer += "I MADE A RECTANGLE YAAYYY"; 
+        }
+        private const string MAKING_RECT_STRING = "<svg width=\"{5}\" height=\"{7}\" x=\"{50}\" y=\"{50}\" xmlns=\"http://www.w3.org/2000/svg\">";
+
         private string Buffer;
         private uint Width;
         private uint Height;
@@ -22,7 +28,7 @@
         }
 
         public string Build() =>
-            string.Format(SVG_HEADER_TEMPLATE, Width, Height)
+            string.Format(SVG_HEADER_TEMPLATE, Width, Height, MAKING_RECT_STRING)
                 + Buffer
                 + SVG_FOOTER;
     }
